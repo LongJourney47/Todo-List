57aafc0d90fc17b74a797d318f5bfd1d4b9a33af
LongJourney47 <95501902+LongJourney47@users.noreply.github.com>
2022-04-26 21:05:08 -0400

    week-05

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,44 +1,5 @@
 import React, { useState } from "react";
 
-// const AddTodoForm = (props) => {
-//   // const [todoTitle, setTodoTilte] = useState('')
-
-//   const [userInput, setUserInput] = useState("");
-
-//   const titleChangeHandler = (event) => {
-//     setUserInput(event.target.value);
-//   };
-
-//   const handleAddTodo = (event) => {
-//     event.preventDefault();
-
-//     // const todoTitle = event.target;
-//     const userData = {
-//       //   ...event,
-//       title: userInput,
-//     };
-//     console.log(userData);
-
-//     setUserInput("");
-//     props.onAddTodo(userInput);
-//   };
-
-//   return (
-//     <form onSubmit={handleAddTodo}>
-//       <label htmlFor="todoTitle">Title: </label>
-//       <input
-//         id="todoTitle"
-//         type="text "
-//         onChange={titleChangeHandler}
-//         value={userInput}
-//       ></input>
-//       <span>
-//         <button type="submit">Add</button>
-//       </span>
-//     </form>
-//   );
-// };
-
 const AddTodoForm = ({ onAddTodo }) => {
   let [todoTitle, setTodoTitle] = useState("");
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,28 +1,6 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-// const todolist = [
-//   { id: "task01", title: "setup web environment" },
-//   { id: "task04", title: "complete assignment" },
-//   { id: "task03", title: "review & refactor" },
-//   { id: "task02", title: "follow instructions" },
-// ];
-// const TodoList = () => {
-//   // let todolist = props.TodoList;
-//   return (
-//     <ul>
-//       {todolist.map((item) => {
-//         // console.log(item.id);
-//         return (
-//           <TodoListItem key={item.id} props={item}>
-//             {" "}
-//           </TodoListItem>
-//         );
-//       })}
-//     </ul>
-//   );
-// };
-
 const TodoList = ({ todoList }) => {
   // console.log(props.todoList);
   // console.log(todo.id);
